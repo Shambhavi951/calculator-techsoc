56a8b6576a469b46eecc3d304e4436ae1e15d55b
Shambhavi951 <shambhavisvijay@gmail.com>
2024-08-16 16:49:11 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,4 @@
+print("CALCULATOR")
+print("Enter the task which you want to perform")
+choice=int(input("""Enter 1 for arithmetic functions
+                    Enter 2 for trigonometric functions"""))
